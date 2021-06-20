c5b8e484361370fae56f3e8757502ca1c6f0f075
Alina Teterkina <teterkina1991@mail.ru>
2021-06-20 12:03:25 +0300

    Задание №2.С корректировкой

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.sandbox;
 
 import static org.testng.Assert.*;
+
 import org.testng.annotations.Test;
 
 public class PointTest {
@@ -10,6 +11,13 @@ public class PointTest {
         Point p1 = new Point(1.0, 1.0);
         Point p2 = new Point(3.0, 1.0);
         double distanceExpected = 2.0;
+        double distanceActual = p1.distanceTo(p2);
+        assertEquals(distanceExpected, distanceActual);
+    }
+    public void testDistance() {
+        Point p1 = new Point(-1.0, 2.0);
+        Point p2 = new Point(-3.0, 4.0);
+        double distanceExpected = 2.8284271247461903;
         double distanceActual = p1.distanceTo(p2);
         assertEquals(distanceExpected, distanceActual);
     }
