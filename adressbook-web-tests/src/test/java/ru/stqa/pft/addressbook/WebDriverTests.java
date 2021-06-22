82d1fb88514e70b5ce5eb26c4539679cffc1aff7
Alina Teterkina <teterkina1991@mail.ru>
2021-06-22 23:02:19 +0300

    Добавлен тест на успешный запуск драйвера

---
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/WebDriverTests.java
@@ -0,0 +1,33 @@
+package ru.stqa.pft.addressbook;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class WebDriverTests {
+
+    private static final String URL_TEST = "http://chsu.ru/";
+
+    private WebDriver driver;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+//        driver = new FirefoxDriver();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void testContactCreationTests() throws Exception {
+        driver.get(URL_TEST);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
