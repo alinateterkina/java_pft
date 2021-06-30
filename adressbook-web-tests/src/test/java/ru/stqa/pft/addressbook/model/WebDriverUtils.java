e0b4422899c1601ea1c2e56eceba5d1e2b1a22a5
Alina Teterkina <teterkina1991@mail.ru>
2021-06-30 21:25:04 +0300

    Задание 7

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/WebDriverUtils.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/WebDriverUtils.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.model;
 public class WebDriverUtils {
 
     public static void setupWebDriverProperties() {
-        System.setProperty("webdriver.chrome.driver", "C:\\wd\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "E:\\devel\\webdrivers\\chromedriver.exe");
+        System.setProperty("webdriver.gecko.driver", "E:\\devel\\webdrivers\\geckodriver.exe");
+        System.setProperty("webdriver.ie.driver", "E:\\devel\\webdrivers\\IEDriverServer.exe");
     }
 }
--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.model.WebDriverUtils;
@@ -7,7 +8,8 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    // TODO : здесь можно выбрать браузер в котором запустятся тесты
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
