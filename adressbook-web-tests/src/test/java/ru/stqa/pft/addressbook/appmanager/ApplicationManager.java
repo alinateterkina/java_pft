0db0bb06cf74fdf9a06a4f112c60b07d2bc1ac9b
Alina Teterkina <teterkina1991@mail.ru>
2021-07-08 12:47:36 +0300

    Доработка задания 7

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,6 +2,7 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
@@ -22,9 +23,13 @@ public class ApplicationManager {
     }
 
     public void init() {
-        if (browser == BrowserType.IE) {
+        if (browser.equals(BrowserType.IE)) {
             wd = new InternetExplorerDriver();
-        } else {
+        }
+        else if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        }
+        else {
             wd = new ChromeDriver();
         }
         //wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
