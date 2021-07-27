b93bab3f2a636c8cff86b4627826f44a56365ede
Alina Teterkina <teterkina1991@mail.ru>
2021-07-27 16:45:31 +0300

    Доработка задания 9

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -16,7 +16,7 @@ public class ContactModificationTests extends TestBase {
     public void ensurePreconditions() {
         app.goTo().homePage();
         if (app.contact().list().size() == 0){
-            app.contact().create(new ContactData("name1", "lastname1","adress1", "111-11-11", "222-22-22", "333-33-33", "1@mail.ru", "2@mail.ru", "3@mail.ru", "test1"), true);
+            app.contact().create(new ContactData().withFirstname("name1"), true);
         }
     }
 
